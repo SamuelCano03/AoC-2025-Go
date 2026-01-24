cca2f952f4f96ee95e54e555ed93e3037aab9710
SamuelCano03 <U202116508@upc.edu.pe>
2026-01-24 12:36:04 -0500

    day 01

--- day01/main.go
+++ day01/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -20,18 +21,59 @@ func read() (string) {
 	}
 	return strings.TrimSpace(line)
 }
-
-func pre() {
-
+func atoi(s string)int{
+	a,_:=strconv.Atoi(s);
+	return a;
+}
+type pr struct{
+	d rune;
+	a int;
+}
+func pre() []pr{
+	mat := make([]pr,0);
+	for {
+		line := read();
+		if line==""{break;}
+		mat = append(mat, pr{rune(line[0]),atoi(line[1:])})
+	}
+	return mat;
 }
 func part1(){
-
+	mat := pre();
+	n := 50
+	cnt :=0;
+	for _,e:=range mat{
+		if e.d=='L'{
+			n -= e.a;
+		}else{
+			n += e.a;
+		}
+		n = ((n%100)+100)%100;
+		if n==0{cnt++;}
+	}
+	ps(cnt);
 }
 func part2(){
-
+	mat := pre();
+	n := 50
+	cnt :=0;
+	for _,e:=range mat{
+		cnt += e.a/100;
+		res:=e.a%100;
+		x:=n;
+		if e.d=='L'{
+			n -= res;
+		}else{
+			n += res;
+		}
+		if n<=0 || n>99{if x!=0{cnt++;}}
+		n = ((n%100)+100)%100;
+		// ps(string(e.d),e.a,x,n,cnt);
+	}
+	ps(cnt);
 }
 func main(){
-	isPart1:=true;
+	isPart1:=false;
 	if isPart1{part1();
 	}    else {part2();}
 }
